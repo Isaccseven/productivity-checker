0c10c990b325b340562f180fc568fee2440689f9
Isaccseven <luca-henn@t-online.de>
2021-07-24 00:04:38 +0200

    init commit

---
+++ src/main/java/com/example/productivitychecker/ProductivityCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.example.productivitychecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductivityCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductivityCheckerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/productivitychecker/ProductivityCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productivitychecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductivityCheckerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
